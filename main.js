7652a3e3c017151ba2b28c05e38afb783a781f21
deliviorr <delivioroosblad@icloud.com>
2025-09-08 16:46:29 +0200

    fixes

--- main.js
+++ main.js
@@ -2,16 +2,16 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.use(express.json()); // Middleware to handle JSON request bodies.
+app.use(express.json()); 
 app.use(express.static('public'));
 
 app.set('view engine', 'ejs');
 
-// Route for handling GET requests.
+
 app.get('/', (req, res) => {
   res.render('home');
 });
 
 app.listen(port, () =>{
-    console.log("http://localhost:3000")
+    console.log("http://localhost:8080")
 });
