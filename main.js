0420154856aefb34ad9010e034f4819ebe8ca74e
deliviorr <delivioroosblad@icloud.com>
2025-09-08 15:38:00 +0200

    eerste view toegevoegd

--- main.js
+++ main.js
@@ -5,12 +5,12 @@ const port = 8080;
 app.use(express.json()); // Middleware to handle JSON request bodies.
 app.use(express.static('public'));
 
+app.set('view engine', 'ejs');
+
 // Route for handling GET requests.
 app.get('/', (req, res) => {
-  res.send('test 123');
+  res.render('home');
 });
-
-app.set('view engine', 'ejs');
 
 app.listen(port, () =>{
     console.log("http://localhost:3000")
